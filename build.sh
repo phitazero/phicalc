6042e2b8201d31010e7bae2be6c2311e4a6556a4
phitazero <kkeke8080@gmail.com>
2025-07-04 23:28:56 +0300

    added build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+if ! which nasm > /dev/null 2>&1; then
+	echo "nasm not found"
+	exit 1
+fi
+
+if ! ls phicalc.c utils.asm > /dev/null 2>&1; then
+	echo "not all source files are found"
+	exit 2
+fi
+
+nasm utils.asm -f elf64 -o utils.o
+gcc phicalc.c utils.o -o phicalc -lm -O2
+rm utils.o
