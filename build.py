7d27dcf88df67a28bdda1c125b5e671d2e980224
phitazero <kkeke8080@gmail.com>
2025-07-08 22:45:47 +0300

    added isInstalled, simple logging and a check if gcc is installed

--- build.py
+++ build.py
@@ -4,27 +4,45 @@ from subprocess import run, Popen, DEVNULL
 from sys import argv
 import json
 
-retcode = Popen(
-	("which", "nasm"),
-	stdout=DEVNULL,
-	stderr=DEVNULL
-).wait()
-if retcode != 0:
-	print("nasm not found")
+def isInstalled(prog):
+	print(f"Checking {prog}... ", end="")
+	retcode = Popen(
+		("which", prog),
+		stdout=DEVNULL,
+		stderr=DEVNULL
+	).wait()
+	found = retcode == 0
+	print("Found" if found else "Not found")
+	return found
+
+if not isInstalled("nasm"):
+	print("fatal: nasm not found")
 	exit(1)
 
-retcode = Popen(
+if not isInstalled("gcc"):
+	print("fatal: gcc not found")
+	exit(2)
+
+print("Checking sources... ", end="")
+
+sourcesFound = Popen(
 	("ls", "phicalc.c", "utils.asm", "keybinds.json"),
 	stdout=DEVNULL
-).wait()
-if retcode != 0:
-	print("not all source files are found")
-	exit(2)
+).wait() == 0
+
+print("Found" if sourcesFound else "Not found")
+
+if not sourcesFound:
+	print("fatal: not all source files are found")
+	exit(3)
+
+print("Parsing keybinds...")
 
 keybinds = json.load(open("keybinds.json", "r"))
 
 definitions = ""
 helpText = ""
+
 
 for macro, keybind in keybinds.items():
 	keyValue = keybind["key"]
@@ -40,10 +58,16 @@ void printKeybinds() {{
 }}
 """
 
+print("Creating keybinds_temp.h...")
 open("keybinds_temp.h", "w").write(codeTemplate.format(definitions, helpText))
 
+print("Assembling utils.asm...")
 run(("nasm", "utils.asm", "-f", "elf64", "-o", "utils.o"))
+print("Compiling phicalc.c, linking with utils.o...")
 run(("gcc", "phicalc.c", "utils.o", "-o", "phicalc", "-lm", "-O2"))
 
 if "-t" not in argv:
+	print("Cleaning up...")
 	run(("rm", "utils.o", "keybinds_temp.h"))
+
+print("Success")
