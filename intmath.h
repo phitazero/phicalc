78957202c5b44e10bb0e60d2a64e086215bc7afa
phitazero <kkeke8080@gmail.com>
2025-07-09 21:39:03 +0300

    renamed o_hi -> io_hi in intmath_div to match the purpose

--- intmath.h
+++ intmath.h
@@ -5,10 +5,10 @@ extern void mulWW(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned)
 extern void mulDD(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned);
 extern void mulQQ(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned);
 
-extern void divBB(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned);
-extern void divWW(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned);
-extern void divDD(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned);
-extern void divQQ(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned);
+extern void divBB(uint64_t* io_lo, uint64_t* io_hi, uint64_t n, uint8_t isSigned);
+extern void divWW(uint64_t* io_lo, uint64_t* io_hi, uint64_t n, uint8_t isSigned);
+extern void divDD(uint64_t* io_lo, uint64_t* io_hi, uint64_t n, uint8_t isSigned);
+extern void divQQ(uint64_t* io_lo, uint64_t* io_hi, uint64_t n, uint8_t isSigned);
 
 void intmath_mul(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned, uint8_t bits) {
 	if (bits == 8)
@@ -21,13 +21,13 @@ void intmath_mul(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned, 
 		mulQQ(io_lo, o_hi, n, isSigned);
 }
 
-void intmath_div(uint64_t* io_lo, uint64_t* o_hi, uint64_t n, uint8_t isSigned, uint8_t bits) {
+void intmath_div(uint64_t* io_lo, uint64_t* io_hi, uint64_t n, uint8_t isSigned, uint8_t bits) {
 	if (bits == 8)
-		divBB(io_lo, o_hi, n, isSigned);
+		divBB(io_lo, io_hi, n, isSigned);
 	else if (bits == 16)
-		divWW(io_lo, o_hi, n, isSigned);
+		divWW(io_lo, io_hi, n, isSigned);
 	else if (bits == 32)
-		divDD(io_lo, o_hi, n, isSigned);
+		divDD(io_lo, io_hi, n, isSigned);
 	else
-		divQQ(io_lo, o_hi, n, isSigned);
+		divQQ(io_lo, io_hi, n, isSigned);
 }
